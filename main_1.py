6e625e3c665907b8fe78f03e10df2daef75f1b08
pini_akerman <piniakerman555@gmail.com>
2026-01-28 13:56:17 +0200

    level-2

--- main_1.py
+++ main_1.py
@@ -1,13 +1,35 @@
-from space_network_lib import *
+import time
+from space_network_lib import SpaceNetwork, SpaceEntity, Packet, TemporalInterferenceError, DataCorruptedError
+
+
 class Satellite(SpaceEntity):
-    def __init__(self , name, distance_from_earth ):
+    def __init__(self, name, distance_from_earth):
         super().__init__(name, distance_from_earth)
-    def receive_signal(self , packet):
-        print(f" [{self.name}]Received: {packet}")
 
-network = SpaceNetwork(level=1)
-Sat1 = Satellite("Satellite_1", 100)
-Sat2 = Satellite("Satellite_2", 200)
+    def receive_signal(self, packet: Packet):
+        print(f"[{self.name}] Received: {packet}. [cite: 69]")
 
-text = Packet("לווין" , Sat1, Sat2)
-network.send(text)
+
+def attempt_transmission(packet):
+    while True:
+        try:
+            network.send(packet)
+            break
+
+        except TemporalInterferenceError:
+            print("...Interference, waiting [cite: 15]")
+            time.sleep(2)
+
+        except DataCorruptedError:
+            print("...corrupted, retrying [cite: 20]")
+
+
+if __name__ == "__main__":
+    network = SpaceNetwork(level=2)
+
+    sat1 = Satellite("Sat1", 100)
+    sat2 = Satellite("Sat2", 200)
+
+    test_packet = Packet("Hello Space!", sat1, sat2)
+
+    attempt_transmission(test_packet)
